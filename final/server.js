56787d2177a35af14de28099ff96acf0b9de99a5
shannonphu <shannonphu@sbcglobal.net>
2017-11-13 21:21:44 -0800

    passed audio data to home.hbs and render audio player

--- final/server.js
+++ final/server.js
@@ -5,14 +5,15 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
+var currentSongs = initialSongs();
+
 app.listen(3000, (request, response) => {
 	console.log("Server is listening on port 3000. Go to http://localhost:3000/");
 });
 
 app.get("/", (request, response) => {
 	response.render("home", {
-		title: "Insert Title Here",
-		content: "Hello, World"
+		songs: currentSongs
 	})
 });
 
